e95b334e350889084af439dd9296da1cb178fdc3
Meng <meng.li@eroad.com>
2017-05-23 18:18:34 +1200

    Customize react-toolbox

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
+import Button from 'react-toolbox/lib/button/Button';
 import logo from './logo.svg';
 import './App.css';
+import theme from './assets/theme';
+import './assets/theme.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+            <Button icon="add" disabled />
+            <Button label="submit" />
+            <Button label="submit" primary />
+            <Button label="submit" primary raised />
+            <Button icon="check" label="OK" accent />
+            <Button icon="add" label="submit" accent raised />
+            <Button icon="add" floating />
+            <Button icon="check" floating primary />
+            <Button icon="add" floating accent />
+            <Button icon="add" floating accent mini />
+          </div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </ThemeProvider>
     );
   }
 }
